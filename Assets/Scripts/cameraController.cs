ee63926066bbd8933030d779bda7bfd8f29b1b10
aluno <aluno@unipe.local>
2019-09-06 17:17:23 -0300

    teste git

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    public GameObject       player;
+    bool                    followPlayer = true;
+    //public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followPlayer == true)
+        {
+            cameraFollowPlayer();
+        }
+    }
+
+    void cameraFollowPlayer()
+    {
+        Vector3 newPosition = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+        this.transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/mouseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseController : MonoBehaviour
+{
+    Vector3 mousePoint;
+    Rigidbody2D rb;
+    Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateCamera();
+    }
+
+    void rotateCamera()
+    {
+        mousePoint = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, (Input.mousePosition.z - camera.transform.position.z)));
+        rb.transform.eulerAngles = new Vector3(0,0, Mathf.Atan2((mousePoint.y - transform.position.y), (mousePoint.x - transform.position.x)) * Mathf.Rad2Deg);
+    }
+}
